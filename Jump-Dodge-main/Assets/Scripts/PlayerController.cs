0331748ac5dba75cb3f8d1265eaa33eb7de043aa
B00130581 <B00130581@mytudublin.ie>
2022-12-16 16:43:17 +0000

    update

---
+++ Jump-Dodge-main/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 5;
+    public float xRange = 24;
+    public float rightSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+    public bool gameOver = false;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * rightSpeed * horizontalInput);
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+        }
+    }
+
+}
